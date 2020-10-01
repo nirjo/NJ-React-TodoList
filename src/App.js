8957ec142777db9af38ba099eb78174d86edb21b
Nirmalraj Joseph <nirmalraj.joseph86@gmail.com>
2020-10-01 19:07:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Form";
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>Ns's React Todo List</h1>
+    </header>
+   <Form />
+   <TodoList />
     </div>
   );
 }
